c139d38d902524d87d7f537ccb9d31b5b67a193e
cmd16 <catherine.dejager10@gmail.com>
2018-03-13 20:57:08 -0400

    Fixed some syntax errors

--- FreqDist.pm
+++ FreqDist.pm
@@ -19,73 +19,86 @@ sub new {
 }
 
 sub get_types {
+    my $self = @_;
     return $self->{_types};
 }
 
 sub get_tokens {
+    my $self = @_;
     return $self->{_tokens};
 }
 
 sub get_hash {
+    my $self = @_;
     return $self->{_hash};
 }
 
-sub get_count($token) {
+sub get_count {
+    my ($self, $token) = @_;
     return $self->{_hash}{$token};
 }
 
-sub get_prob_token($token) {
+sub get_prob_token {
+    my ($self, $token) = @_;
     return $self->{_hash}{$token} / $self->{_tokens};
 }
 
-sub get_prob_type($type) {
+sub get_prob_type {
+    my ($self, $type) = @_;
     return 1 / $self->{_types};
 }
 
-sub get_normalized_freq($token) {
+sub get_normalized_freq {
+    my ($self, $token) = @_;
     return $self->{_hash}{$token} / $self->{_tokens} * 1000000;
 }
 
-sub add_token($token) {
+sub add_token {
+    my ($self, $token) = @_;
     $self->{_hash}{$token} += 1;
     $self->{_tokens} += 1;
     $self->{_types} = keys % $self->{_hash};
 }
 
-sub remove_type($type) {
+sub remove_type {
+    my ($self, $type) = @_;
     my $tokens = $self->{_hash}{$type};
     delete $self->{_hash}{$type};
     $self->{_tokens} -= $tokens;
     $self->{_types} -= 1;
 }
 
-sub add_token_freq($token, $freq) {
+sub add_token_freq {
+    my ($self, $token, $freq) = @_;
     $self->{_hash}{$token} += $freq;
     $self->{_tokens} += $freq;
     $self->{_types} = keys % $self->{_hash};
 }
 
 sub get_max {
+    my $self = @_;
     my $max_token;
     my $max_freq = 0;
     keys $self->{_hash}; # reset the internal iterator so a prior each() doesn't affect the loop
     while(my($token, $freq) = each $self->{_hash}) {
-        if ($freq > max_freq) {
-            max_token = $token;
-            max_freq = $freq;
+        if ($freq > $max_freq) {
+            $max_token = $token;
+            $max_freq = $freq;
         }
     }
     return ($max_token, $max_freq)
 }
 
 sub clear_hash {
+    my $self = @_;
     $self->{_hash} = {};
     $self->{_types} = 0;
     $self->{_tokens} = 0;
     $self->{_keyword_dicts} = ();
 }
 
-sub out_to_txt($filename) {
+sub out_to_txt {
+    my ($self, $filename) = @_;
     open(my $out, ">", $filename) or die "Couldn't open $filename, $!";
     printf $out "#Word types: %d\n", $self->{_types};
     printf $out "#Word tokens: %d\n", $self->{_tokens};
@@ -95,14 +108,15 @@ sub out_to_txt($filename) {
     close $out;
 }
 
-sub open_from_txt($filename) {
+sub open_from_txt {
+    my ($self, $filename) = @_;
     # clear out the old values
     foreach my $key ($self->{_hash}) {
         $self->remove_type($key);
     }
     # read in the new values
     open(my $in, "<", $filename) or die "Couldn't open $filename, $!";
-    while($line = <$in>) {
+    while(my $line = <$in>) {
         next if $. < 2;  # skip first 2 lines which have types and tokens (https://stackoverflow.com/questions/14393295/best-way-to-skip-a-header-when-reading-in-from-a-text-file-in-perl)
         my @word_and_freq = split(/\t/, $line);
         $self->add_token_freq($word_and_freq[0], $word_and_freq[1]);
@@ -110,28 +124,33 @@ sub open_from_txt($filename) {
     close $in;
 }
 
-sub update($other) {  # TODO: check declaration
-    %other_hash = $other->get_hash(); # reset the internal iterator so a prior each() doesn't affect the loop
+sub update {  # TODO: check declaration
+    my ($self, $other) = @_;
+    my %other_hash = $other->get_hash(); # reset the internal iterator so a prior each() doesn't affect the loop
     while(my($token, $freq) = each $other_hash) {
         $self->add_token_freq($token, $freq);
     }
 }
 
-sub keyword_analysis($other, $p) {  # TODO: check declaration
+sub keyword_analysis {  # TODO: check declaration
+    my ($self, $other, $p) = @_;
     my $crit;
-    switch($p) {
-        case 0.05 { $crit = 3.84; }
-        case 0.01 { $crit = 6.63; }
-        case 0.001 { $crit = 10.83; }
-        case 0.0001 { $crit = 15.13; }
-        else { $crit = 0; }
-    }
+    # switch($p) {
+    #     case 0.05 { $crit = 3.84; }
+    #     case 0.01 { $crit = 6.63; }
+    #     case 0.001 { $crit = 10.83; }
+    #     case 0.0001 { $crit = 15.13; }
+    #     else { $crit = 0; }
+    # }
+    $crit = 0;  # TODO: change later
     my %keyword_hash = {};
     keys $self->{_hash}; # reset the internal iterator so a prior each() doesn't affect the loop
     while(my($token, $freq1) = each $self->{_hash}) {
         my $freq2 = $other->get_count($token);
         my $norm1 = $self->get_normalized_freq($token);
         my $norm2 = $other->get_normalized_freq($token);
+        my $tokens1 = $self->get_tokens();
+        my $tokens2 = $other->get_tokens();
         my $num = ($freq1 + $freq2) / ($tokens1 + $tokens2);
         my $E1 = $tokens1 * $num;
         my $E2 = $tokens2 * $num;
@@ -139,12 +158,13 @@ sub keyword_analysis($other, $p) {  # TODO: check declaration
         if ($keyness < $crit) {
             next;
         }
-        %keyword_hash{$token} = {'keyness'=> $keyness, 'freq1'=>freq1, 'norm1'=>$norm1, 'freq2'=>$freq2, 'norm2'=>$norm2};
+        %keyword_hash{$token} = {'keyness'=> $keyness, 'freq1'=>$freq1, 'norm1'=>$norm1, 'freq2'=>$freq2, 'norm2'=>$norm2};
     }
     push($self->{_keyword_dicts}, %keyword_hash);  # TODO: change this later?
 }
 
-sub print_keywords(@filenames, @indexes) {
+sub print_keywords {
+    my ($self, @filenames, @indexes) = @_;
     foreach my $index (@indexes) {
         my $filename = @filenames[$index];
         open(my $out, ">", $filename) or die "Couldn't open $filename, $!";
